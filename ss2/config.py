d01b363d91ac933378becb28150215344d9044d3
Leo Scott <leo@sqi-inc.com>
2016-08-03 17:29:12 -0500

    Many improvements to config, plus tests

--- ss2/config.py
+++ ss2/config.py
@@ -35,30 +35,104 @@ except ImportError:
 DEFAULT_CONFIG = os.path.join(os.path.dirname(__file__), 'defaults.cfg')
 DEFAULT_FILES = ['ss2.cfg']
 
-def read_config(files=None, section="Core"):
+class AttrDict(dict):
+    "An object that allows attrDict.foo or attrDict['foo']"
+    __getattr__ = dict.__getitem__
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
+    def __init__(self, *args, **kwargs):
+        super(AttrDict, self).__init__(*args, **kwargs)
+        self.parser = None
+
+def get_parser(files=None):
     "Read config files for SS2"
     files = files or DEFAULT_FILES
 
     parser = ConfigParser()
     parser.readfp(open(DEFAULT_CONFIG))
     parser.read(files)
+    return parser
 
-    class AttrDict(dict):
-        "An object that allows attrDict.foo or attrDict['foo']"
-        __getattr__ = dict.__getitem__
-        __setattr__ = dict.__setitem__
-        __delattr__ = dict.__delitem__
+def read_config(files=None, section="Core"):
+    "Helper to get an AttrDict of the config from the specified section"
+    parser = get_parser(files)
+    items = get_section(parser, section)
+    config = parse_types(items, parser)
+    return config
 
+def get_section(parser, section):
+    "Get resolved items from the appropriate section/subsection"
+    items = []
+    # Use DEFAULTS rather than DEFAULT so we don't get ConfigParser's default
+    # behavior when we finally get the items in the full section path
+    items += parser.items("DEFAULTS")
+    path = section.split("/")
+    if len(path) <= 1:
+        return items + parser.items(section)
+
+    search = []
+    for p in path[:-1]:
+        search.append(p)
+        if parser.has_section("/".join(search)):
+            items += parser.items("/".join(search))
+        if parser.has_section("/".join(search + ["DEFAULTS"])):
+            items += parser.items("/".join(search + ["DEFAULTS"]))
+
+    items += parser.items(section)
+    return items
+
+def get_subsections(parser, section, deep=False):
+    "Get a list of subsections for the specified section"
+    sections = []
+    section_depth = len(section.split("/"))
+    for s in parser.sections():
+        if not deep:
+            depth = len(s.split("/"))
+            if depth > section_depth + 1:
+                continue
+        if not s.startswith(section + "/"):
+            continue
+        if s.endswith("/DEFAULTS"):
+            continue
+        sections.append(s)
+
+    return sections
+
+def parse_types(items, parser=None):
+    "Parse items list in to an AttrDict with automatic type conversion"
     config = AttrDict()
-    for k, v in parser.items(section):
+    config.parser = parser
+
+    for k, v in items:
         try:
             if "." in v:
                 v = float(v)
+            elif v in ["true", "yes", "on"]:
+                v = True
+            elif v in ["false", "no", "off"]:
+                v = False
             else:
                 v = int(v, 0)
         except ValueError:
             pass
 
-        config[k] = v
+        if "." in k:
+            # Allow dictionary paths in config. For example:
+            #   foo.bar: baz
+            # Can be accessed as `config.foo.bar`
+            path = k.split(".")
+            current = config
+            for p in path[:-1]:
+                # If needed, reset current path to an empty AttrDict
+                if p in current and isinstance(current[p], AttrDict):
+                    current = current[p]
+                else:
+                    current[p] = AttrDict()
+                    current = current[p]
+                    config.parser = parser
+
+            current[path[-1]] = v
+        else:
+            config[k] = v
 
     return config
---
+++ ss2/tests/test_config.py
@@ -0,0 +1,147 @@
+# Copyright (c) 2016 Inside OpenFlow
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation
+# files (the "Software"), to deal in the Software without
+# restriction, including without limitation the rights to use, copy,
+# modify, merge, publish, distribute, sublicense, and/or sell copies
+# of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+"Test the config loader"
+
+import unittest
+import io
+import six
+from textwrap import dedent
+from ss2 import config
+
+if six.PY2:
+    import ConfigParser as configparser
+else:
+    import configparser
+
+# pylint: disable=C0111
+
+class ParseTypesTestCase(unittest.TestCase):
+    def test_int(self):
+        items = [("foo", "12345"), ("bar", "0xff")]
+        parsed = config.parse_types(items)
+        self.assertEqual(parsed.foo, 12345)
+        self.assertEqual(parsed.bar, 0xff)
+
+    def test_float(self):
+        items = [("foo", "1.24"), ("bar", "invalid.float")]
+        parsed = config.parse_types(items)
+        self.assertAlmostEqual(parsed.foo, 1.24)
+        self.assertEqual(parsed.bar, "invalid.float")
+
+    def test_bool(self):
+        items = [("foo", "true"), ("bar", "no")]
+        parsed = config.parse_types(items)
+        self.assertIs(parsed.foo, True)
+        self.assertIs(parsed.bar, False)
+
+    def test_dictkey(self):
+        items = [
+            ("foo.bar", "123"),
+            ("foo.baz", "no"),
+        ]
+        parsed = config.parse_types(items)
+        self.assertEqual(parsed.foo.bar, 123)
+        self.assertEqual(parsed.foo.baz, False)
+
+        items = [
+            ("foo.bar", "123"),
+            ("foo", "other")
+        ]
+        parsed = config.parse_types(items)
+        self.assertEqual(parsed.foo, "other")
+
+        items = [
+            ("foo.bar", "123"),
+            ("foo.baz", "no"),
+            ("foo", "other"), # resets the dict
+            ("foo.bar", "321")
+        ]
+        parsed = config.parse_types(items)
+        self.assertEqual(parsed.foo.bar, 321)
+        self.assertFalse("baz" in parsed.foo)
+
+class SubsectionTestCase(unittest.TestCase):
+    def setUp(self):
+        # Example intentionally not in order
+        example = """
+        [ACL]
+        foo.baz: ACL
+        foo.acl: ACL
+        [ACL/Rule A]
+        foo.bar: ACL-Rule-A
+        [ACL/Rule B]
+        foo.baz: ACL-Rule-B
+        [DEFAULTS]
+        foo.bar: Default
+        foo.baz: Default
+        [Deep/Subsection/Without/Parents]
+        foo.baz: Deep
+        [ACL/DEFAULTS]
+        foo.bar: ACL-Default
+        foo.baz: ACL-Default
+        """
+
+        self.parser = config.ConfigParser()
+        if six.PY2:
+            self.parser.readfp(io.BytesIO(dedent(example)))
+        else:
+            self.parser.read_string(example)
+
+    def test_no_subsection(self):
+        items = config.get_section(self.parser, "ACL")
+        c = config.parse_types(items)
+        self.assertEqual(c.foo.bar, "Default")
+        self.assertEqual(c.foo.baz, "ACL")
+
+    def test_shallow_subsection(self):
+        items = config.get_section(self.parser, "ACL/Rule A")
+        c = config.parse_types(items)
+        self.assertEqual(c.foo.bar, "ACL-Rule-A")
+        self.assertEqual(c.foo.baz, "ACL-Default")
+
+        items = config.get_section(self.parser, "ACL/Rule B")
+        c = config.parse_types(items)
+        self.assertEqual(c.foo.bar, "ACL-Default")
+        self.assertEqual(c.foo.baz, "ACL-Rule-B")
+
+    def test_deep_subsection(self):
+        items = config.get_section(self.parser, "Deep/Subsection/Without/Parents")
+        c = config.parse_types(items)
+        self.assertEqual(c.foo.bar, "Default")
+        self.assertEqual(c.foo.baz, "Deep")
+
+    def test_no_section(self):
+        with self.assertRaises(configparser.NoSectionError):
+            config.get_section(self.parser, "Deep/Subsection")
+
+    def test_get_subsections(self):
+        subs = config.get_subsections(self.parser, "ACL")
+        self.assertEqual(len(subs), 2)
+        self.assertNotIn("ACL/DEFAULTS", subs)
+
+    def test_get_subsections_deep(self):
+        subs = config.get_subsections(self.parser, "Deep/Subsection")
+        self.assertEqual(len(subs), 0)
+
+        subs = config.get_subsections(self.parser, "Deep/Subsection", True)
+        self.assertEqual(len(subs), 1)
+        self.assertIn("Deep/Subsection/Without/Parents", subs)
